c83e56c6ddea6ec241d78c7f23714eaf2000408c
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-08-29 16:46:37 +0800

    refactor: udpate mui library and standardized mui styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,44 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
-import SignupPage from './components/SignupPage';
-import {Box} from "@mui/material";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import { createStyles, makeStyles } from "@material-ui/core";
+
+import SignupPage from "./pages/SignUp/SignupPage";
+import { Box } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    rootBox: {
+      display: "flex",
+      flexDirection: "column",
+      padding: "4rem",
+    },
+  })
+);
 
 function App() {
-    return (
-        <div className="App">
-            <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
-                <Router>
-                    <Routes>
-                        <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
-                        <Route path="/signup" element={<SignupPage/>}/>
-                    </Routes>
-                </Router>
-            </Box>
-        </div>
-    );
+  const classes = useStyles();
+
+  return (
+    <div className="App">
+      <Box className={classes.rootBox}>
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<Navigate replace to="/signup" />}
+            ></Route>
+            <Route path="/signup" element={<SignupPage />} />
+          </Routes>
+        </Router>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
