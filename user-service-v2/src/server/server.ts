82eb1e7839ed98baa628487f04458b66256807e0
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-03 19:41:07 +0800

    setup: prettier

--- user-service-v2/src/server/server.ts
+++ user-service-v2/src/server/server.ts
@@ -2,11 +2,10 @@ import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 const port: number = 8000;
-
-app.get("/", (req: Request, res: Response) => {
-    res.send(`Express + TypeScript Server`);
+app.get("/", (_req: Request, res: Response) => {
+  res.send(`Express + TypeScript Server`);
 });
 
 app.listen(port, () => {
-    console.log(`[question-service] Server is listening on Port ${port}`);
+  console.log(`[user-service] Server is listening on Port ${port}`);
 });
