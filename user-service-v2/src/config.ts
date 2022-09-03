deb02b801f49a3df4d1ae1c8e8e9db9b089aabf4
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-04 01:38:36 +0800

    setup: add env variable setup

---
+++ user-service-v2/src/config.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+
+export const port = Number(process.env.API_PORT);
+export const db_host = String(process.env.DB_HOST);
+export const db_port = Number(process.env.DB_PORT);
+export const db_name = String(process.env.DB_NAME);
+export const db_user = String(process.env.DB_USER);
+export const db_password = String(process.env.DB_PASSWORD);
