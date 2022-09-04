1b7d97acfda27e618b9fd3c8a65a04fa1f477962
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-04 12:22:19 +0800

    setup: sequelize convert dal to repositories

---
+++ user-service-v2/src/db/repositories/interfaces/UserRepositoryInterface.ts
@@ -0,0 +1,14 @@
+import { UserInput, UserOutput } from "../../models/User";
+import { GetAllUsersFilters } from "../filters";
+
+/**
+ * A user repository that handles access to the data store of Urls.
+ */
+
+export interface UserRepositoryInterface {
+  create(payload: UserInput): Promise<UserOutput>;
+  update(id: number, payload: Partial<UserInput>): Promise<UserOutput>;
+  getById(id: number): Promise<UserOutput>;
+  deleteById(id: number): Promise<boolean>;
+  getAll(filters?: GetAllUsersFilters): Promise<UserOutput[]>;
+}
