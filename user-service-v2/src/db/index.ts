df919dc2a5d348ecefe9c93d812ed3289d1a0415
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-04 11:46:02 +0800

    setup: sequelize libraries, db models and dbInit

---
+++ user-service-v2/src/db/index.ts
@@ -0,0 +1,14 @@
+import User from "./models/User";
+import { DEV_ENV } from "../config";
+
+const isDev = DEV_ENV === "development";
+
+const dbInit = () => {
+  // sync accepts alter or force options
+  // force option forces the recreation of a table
+  // alter option creates the table if it does not exist or updates
+  // the table to match the attributes defined in the model
+  User.sync({ alter: isDev });
+};
+
+export default dbInit;
