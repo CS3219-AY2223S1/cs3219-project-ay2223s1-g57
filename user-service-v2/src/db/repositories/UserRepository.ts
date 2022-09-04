dd09155aa71105440678ea29430bf2bd0929c0fa
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-04 12:22:19 +0800

    setup: sequelize convert dal to repositories

---
+++ user-service-v2/src/db/repositories/UserRepository.ts
@@ -0,0 +1,56 @@
+import { Op } from "sequelize";
+import User, { UserInput, UserOutput } from "../models/User";
+import { GetAllUsersFilters } from "./filters";
+import { UserRepositoryInterface } from "./interfaces/UserRepositoryInterface";
+export class UserRepository implements UserRepositoryInterface {
+  public create = async (payload: UserInput): Promise<UserOutput> => {
+    const user = await User.create(payload);
+    return user;
+  };
+
+  public update = async (
+    id: number,
+    payload: Partial<UserInput>,
+  ): Promise<UserOutput> => {
+    const user = await User.findByPk(id);
+    if (!user) {
+      // @todo, throw custom error
+      throw new Error("user not found");
+    }
+
+    const updatedUser = await (user as User).update(payload);
+    return updatedUser;
+  };
+
+  public getById = async (id: number): Promise<UserOutput> => {
+    const user = await User.findByPk(id);
+    if (!user) {
+      // @todo, throw custom error
+      throw new Error("user not found");
+    }
+    return user;
+  };
+
+  public deleteById = async (id: number): Promise<boolean> => {
+    const deletedUserCount = await User.destroy({
+      where: { id },
+    });
+    return !!deletedUserCount;
+  };
+
+  public getAll = async (
+    filters?: GetAllUsersFilters,
+  ): Promise<UserOutput[]> => {
+    return User.findAll({
+      where: {
+        ...(filters?.isDeleted && { deletedAt: { [Op.not]: null } }),
+      },
+      // Adding the paranoid: true option to the findAll model method
+      // includes the soft-deleted records with deletedAt set in the result.
+      // Otherwise, the results exclude soft deleted records by default.
+      ...((filters?.isDeleted || filters?.includeDeleted) && {
+        paranoid: true,
+      }),
+    });
+  };
+}
