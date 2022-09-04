91bfd04f7db1cc6743a753b94baa91a4a6d00a14
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-04 12:52:00 +0800

    setup: sequelize service

---
+++ user-service-v2/src/api/services/UserService.ts
@@ -0,0 +1,32 @@
+import { UserInput, UserOutput } from "../../db/models/User";
+import { GetAllUsersFilters } from "../../db/repositories/filters";
+import { UserServiceInterface } from "./interfaces/UserServiceInterface";
+import { UserRepositoryInterface } from "../../db/repositories/interfaces/UserRepositoryInterface";
+
+export class UserService implements UserServiceInterface {
+  private userRepository: UserRepositoryInterface;
+
+  constructor(_userRepository: UserRepositoryInterface) {
+    this.userRepository = _userRepository;
+  }
+
+  create(payload: UserInput): Promise<UserOutput> {
+    return this.userRepository.create(payload);
+  }
+
+  update(id: number, payload: Partial<UserInput>): Promise<UserOutput> {
+    return this.userRepository.update(id, payload);
+  }
+
+  getById(id: number): Promise<UserOutput> {
+    return this.userRepository.getById(id);
+  }
+
+  deleteById(id: number): Promise<boolean> {
+    return this.userRepository.deleteById(id);
+  }
+
+  getAll(filters?: GetAllUsersFilters | undefined): Promise<UserOutput[]> {
+    return this.getAll(filters);
+  }
+}
---
+++ user-service-v2/src/api/services/interfaces/UserServiceInterface.ts
@@ -0,0 +1,15 @@
+import { UserInput, UserOutput } from "../../../db/models/User";
+import { GetAllUsersFilters } from "../../../db/repositories/filters";
+
+/**
+ * A user service that connects the repository to the controller
+ * Handles the dependencies
+ */
+
+export interface UserServiceInterface {
+  create(payload: UserInput): Promise<UserOutput>;
+  update(id: number, payload: Partial<UserInput>): Promise<UserOutput>;
+  getById(id: number): Promise<UserOutput>;
+  deleteById(id: number): Promise<boolean>;
+  getAll(filters?: GetAllUsersFilters): Promise<UserOutput[]>;
+}
