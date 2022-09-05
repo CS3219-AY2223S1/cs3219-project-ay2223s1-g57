09428489302bb34310d5f4aa3a7d70671e1314cd
Esmanda Wong <esmanda3w@gmail.com>
2022-09-06 01:31:25 +0800

    resolve cors error

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,74 +1,84 @@
-import express from 'express';
-import cors from 'cors';
-import { createServer } from 'http';
-import {Server} from 'socket.io'
-import { createMatch, leaveRoom, disconnect, foundMatchWithin30s } from './controller/match-controller.js';
-import { syncBuiltinESMExports } from 'module';
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import {
+  createMatch,
+  leaveRoom,
+  disconnect,
+  foundMatchWithin30s,
+} from "./controller/match-controller.js";
+import { syncBuiltinESMExports } from "module";
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
-app.options('*', cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // config cors so that front-end can use
+app.options("*", cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World from matching-service');
+app.get("/", (req, res) => {
+  res.send("Hello World from matching-service");
 });
 
-const httpServer = createServer(app)
-const io = new Server(httpServer, {})
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-    console.log("connected " + String(socket.id))
+  console.log("connected " + String(socket.id));
 
-    socket.on("match", (difficulty) => {
-        createMatch(socket, difficulty).then((params) => {
-            const roomId = params[0]
-            const matchFound = params[1]
+  socket.on("match", (difficulty) => {
+    createMatch(socket, difficulty).then((params) => {
+      const roomId = params[0];
+      const matchFound = params[1];
 
-            if (matchFound) {
-                io.to(roomId).emit("matchSuccess", {
-                    roomId: roomId
-                })
-                return
-            } 
-    
-            socket.emit("searching", {
-                roomId: roomId
-            })
-            foundMatchWithin30s(socket.id).then((found) => {
-                if (!found) {
-                    socket.leave(roomId)
-                    socket.emit("matchFail", {
-                        text: "Match not found after 30 seconds"
-                    })
-                }
-            })
-        })
-    })
+      if (matchFound) {
+        io.to(roomId).emit("matchSuccess", {
+          roomId: roomId,
+        });
+        return;
+      }
 
-    socket.on("leaveRoom", (roomId) => {
-        leaveRoom(socket.id, roomId).then(() => {
-            socket.leave(roomId)
-            io.to(roomId).emit("matchLost", {
-                text: "Partner has left the room"
-            })
-            io.in(roomId).socketsLeave(roomId)
-        })
-    })
+      socket.emit("searching", {
+        roomId: roomId,
+      });
+      foundMatchWithin30s(socket.id).then((found) => {
+        if (!found) {
+          socket.leave(roomId);
+          socket.emit("matchFail", {
+            text: "Match not found after 30 seconds",
+          });
+        }
+      });
+    });
+  });
 
-    socket.on("disconnect", () => {
-        disconnect(socket.id).then(roomId => {
-            if (roomId) {
-                socket.leave(roomId)
-                io.to(roomId).emit("matchLost", {
-                    text: "Partner has disconnected"
-                })
-                io.in(roomId).socketsLeave(roomId)
-            }
-        })
-        console.log("disconnected " + String(socket.id))
-    })
-})
+  socket.on("leaveRoom", (roomId) => {
+    leaveRoom(socket.id, roomId).then(() => {
+      socket.leave(roomId);
+      io.to(roomId).emit("matchLost", {
+        text: "Partner has left the room",
+      });
+      io.in(roomId).socketsLeave(roomId);
+    });
+  });
+
+  socket.on("disconnect", () => {
+    disconnect(socket.id).then((roomId) => {
+      if (roomId) {
+        socket.leave(roomId);
+        io.to(roomId).emit("matchLost", {
+          text: "Partner has disconnected",
+        });
+        io.in(roomId).socketsLeave(roomId);
+      }
+    });
+    console.log("disconnected " + String(socket.id));
+  });
+});
 
 httpServer.listen(8001);
