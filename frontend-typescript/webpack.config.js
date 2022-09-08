870d748ca8d6f36faeacbe155fde69a95e09d735
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-09 01:39:14 +0800

    setup: react, babel, webpack

---
+++ frontend-typescript/webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+const path = require('path')
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {       
+      directory: path.resolve(__dirname, './dist')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+}
