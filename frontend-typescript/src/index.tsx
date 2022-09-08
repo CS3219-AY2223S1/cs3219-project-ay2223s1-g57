2b3df72dbe689c7e86f0b71f9fef8f2564139971
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-09 02:50:25 +0800

    setup: eslint

--- frontend-typescript/src/index.tsx
+++ frontend-typescript/src/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './App'
 const rootNode = document.getElementById('app')
 
-if(rootNode){
-  createRoot(rootNode)
-    .render(<App />);
+if (rootNode) {
+  createRoot(rootNode).render(<App />)
 }
