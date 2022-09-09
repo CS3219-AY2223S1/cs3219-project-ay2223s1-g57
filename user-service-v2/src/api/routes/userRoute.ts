c422042f8d92f3e6fd2449d6ff2b7b991623871c
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-09 16:08:16 +0800

    refactor: clean api to put controller and middleware abstraction

--- user-service-v2/src/api/routes/userRoute.ts
+++ user-service-v2/src/api/routes/userRoute.ts
@@ -1,5 +1,4 @@
-import { Router, Request, Response } from "express";
-import { CreateUserDTO } from "../dto/user.dto";
+import { Router } from "express";
 import * as userController from "../controllers/user/index";
 const userRouter = Router();
 
@@ -15,20 +14,10 @@ userRouter.delete(":/slug", () => {
   // delete user
 });
 
-userRouter.post("/", async (req: Request, res: Response) => {
-  const payload: CreateUserDTO = req.body;
-  try {
-    const isDuplicate = await userController.checkIfUserExists(
-      payload.username,
-    );
-    if (isDuplicate) {
-      return res.status(409).send("username is already taken here");
-    }
-    await userController.create(payload);
-    return res.status(201).send("user created");
-  } catch (error) {
-    return res.status(409).send("username is already taken");
-  }
-});
+userRouter.post(
+  "/",
+  userController.userDuplicateMiddleware,
+  userController.createUser,
+);
 
 export default userRouter;
