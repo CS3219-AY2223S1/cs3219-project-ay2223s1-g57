8fe176ce87d4bca415284c11d4bae7c7fc39f57c
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-09 16:08:16 +0800

    refactor: clean api to put controller and middleware abstraction

--- user-service-v2/src/api/controllers/user/index.ts
+++ user-service-v2/src/api/controllers/user/index.ts
@@ -1,15 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+
 import { UserServiceInterface } from "../../../db/services/interfaces/UserServiceInterface";
 import { UserService } from "../../../db/services/UserService";
 import { CreateUserDTO } from "../../dto/user.dto";
-import { User } from "../../object-interfaces";
 import * as mapper from "./mapper";
 
 const userService: UserServiceInterface = new UserService();
 
-export const create = async (payload: CreateUserDTO): Promise<User> => {
-  return mapper.toUser(await userService.create(payload));
+export const createUser = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  const payload: CreateUserDTO = req.body;
+  try {
+    mapper.toUser(await userService.create(payload));
+    res.status(201).send("user created");
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
-export const checkIfUserExists = async (username: string): Promise<Boolean> => {
-  return userService.checkIfUserExists(username);
+export const userDuplicateMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const username: string = req.body.username;
+  const isDuplicate = await userService.checkIfUserExists(username);
+  if (isDuplicate) {
+    res.status(409).send("username is already taken here");
+    return;
+  }
+  next();
 };
