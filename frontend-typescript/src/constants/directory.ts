7f794f29a4ca2a445a46cb7e014c9d1a38b4178c
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-11 18:14:47 +0800

    feat: frontend: change password dialog

--- frontend-typescript/src/constants/directory.ts
+++ frontend-typescript/src/constants/directory.ts
@@ -1,3 +1,4 @@
 export const SIGN_UP = '/signup'
 export const LOG_IN = '/login'
 export const HOME = '/home'
+export const SETTINGS = '/settings'
---
+++ frontend-typescript/src/pages/Settings/SettingsPage.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+import {
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Typography,
+  Grid,
+} from '@mui/material'
+import ChangePasswordDialog from './ChangePasswordDialog'
+
+const SettingsPage = () => {
+  const [isPasswordDialogActive, setIsPasswordDialogActive] = useState(false)
+
+  const handleClickOpen = () => {
+    setIsPasswordDialogActive(true)
+  }
+
+  const handleClose = () => {
+    setIsPasswordDialogActive(false)
+  }
+  return (
+    <>
+      <ChangePasswordDialog
+        open={isPasswordDialogActive}
+        handleClose={handleClose}
+      />
+      <Grid sx={{ padding: '15px', alignItems: 'center' }}>
+        <Card sx={{ maxWidth: 345, paddingTop: '5px', paddingBottom: '5px' }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Account Information
+            </Typography>
+
+            <Typography variant="h6">Username</Typography>
+
+            <Typography variant="body2" color="text.secondary">
+              UserName
+            </Typography>
+            <Divider variant="fullWidth" />
+
+            <Typography variant="h6">Password</Typography>
+
+            <Button onClick={handleClickOpen} size="small">
+              Change Password
+            </Button>
+            <Divider variant="fullWidth" />
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid sx={{ padding: '15px' }}>
+        <Card sx={{ maxWidth: 345, paddingTop: '5px', paddingBottom: '5px' }}>
+          <CardContent>
+            <Button sx={{ color: '#ff0000' }} size="small">
+              Delete Account
+            </Button>
+          </CardContent>
+        </Card>
+      </Grid>
+    </>
+  )
+}
+
+export default SettingsPage
