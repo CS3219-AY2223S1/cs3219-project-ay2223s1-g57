c5d2deb25851b30495e2e9c94156cd56c2779044
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-09-11 13:10:39 +0800

    feat: authentication with cookies

--- frontend-typescript/src/pages/App/App.tsx
+++ frontend-typescript/src/pages/App/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import React, { useState } from 'react'
+import { Box, SxProps } from '@mui/material'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
+import { SIGN_UP, LOG_IN, HOME } from '../../constants/directory'
+import { useAuth } from '../../context/AuthContext'
+import NotFound from './NotFound'
 import LoginPage from '../LogIn/LoginPage'
 import SignupPage from '../SignUp/SignUpPage'
 import HomePage from '../Home/HomePage'
 
+const classProps: { [className: string]: SxProps } = {
+  rootDiv: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '4rem',
+  },
+}
+
 const App: React.FC = () => {
+  const { currentCookie } = useAuth()
+
+  // cookies.set('jimeeee', 'hello', { path: '/' })
+  // const cookies2 = new Cookies()
+
+  // cookies2.set('jimeeee', 'brother', )
+  // cookies2.remove('jimeeee')
+
+  // console.log('cookie')
+
   return (
     <div className="App">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          padding: '4rem',
-        }}
-      >
+      <Box sx={classProps['rootDiv']}>
         <Routes>
-          <Route path="/" element={<Navigate replace to="/login" />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/home" element={<HomePage />} />
+          {currentCookie && (
+            <>
+              <Route path="/" element={<Navigate to={HOME} />} />
+              <Route path={LOG_IN} element={<Navigate to={HOME} />} />
+              <Route path={SIGN_UP} element={<Navigate to={HOME} />} />
+              <Route path="/home" element={<HomePage />} />
+            </>
+          )}
+          {!currentCookie && (
+            <>
+              <Route path="/" element={<Navigate to={LOG_IN} />} />
+              <Route path={LOG_IN} element={<LoginPage />} />
+              <Route path={SIGN_UP} element={<SignupPage />} />
+            </>
+          )}
+          <Route path="*" element={<NotFound />} />
           {/* <Route path="/lobby" element={<LobbyPage />} />
           <Route path="/session" element={<SessionPage />} /> */}
         </Routes>
