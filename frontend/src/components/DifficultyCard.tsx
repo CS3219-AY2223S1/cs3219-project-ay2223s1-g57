e7cd1ff732385281f40398c0b97fa97cf7d9ed53
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-10-16 05:33:29 +0800

    chore: improve ui for home page

---
+++ frontend/src/components/DifficultyCard.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import StarIcon from '@mui/icons-material/Star'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material'
+type DifficultyCardProps = {
+  difficulty: string
+  starCount: number
+}
+
+const DifficultyCard = ({ difficulty, starCount }: DifficultyCardProps) => {
+  return (
+    <Card
+      sx={{
+        paddingTop: '5px',
+        paddingBottom: '5px',
+      }}
+    >
+      <CardContent>
+        <Grid
+          container
+          sx={{
+            direction: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minWidth: 250,
+          }}
+        >
+          <Grid item>
+            <Typography
+              sx={{ paddingBottom: '10px', color: '#2F2F5A' }}
+              variant="body1"
+            >
+              {difficulty}
+            </Typography>
+          </Grid>
+          <Box width="100%" />
+          <Grid item>
+            <ArrowBackIosIcon sx={{ color: '#E56E52' }} fontSize="large" />
+            {[...Array(starCount)].map((num) => (
+              <StarIcon sx={{ color: '#E56E52' }} fontSize="large" />
+            ))}
+            <ArrowForwardIosIcon sx={{ color: '#E56E52' }} fontSize="large" />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default DifficultyCard
