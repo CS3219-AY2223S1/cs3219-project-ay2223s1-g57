de5218cd9c8b8e80a6e6e640af06125707397668
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-10-16 04:37:38 +0800

    chore: improve ui for settings

--- frontend/src/pages/Settings/SettingsPage.tsx
+++ frontend/src/pages/Settings/SettingsPage.tsx
@@ -45,51 +45,100 @@ const SettingsPage = () => {
   }
   return (
     <>
-      <Header enableHeaderButtons={false} />
+      <Header enableHeaderButtons={true} />
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minWidth: '100vh', minHeight: '60vh' }}
+      >
+        {' '}
+        <ChangePasswordDialog
+          open={isPasswordDialogActive}
+          handleClose={handlePasswordDialogClose}
+        />
+        <DeleteUserDialog
+          open={isDeleteDialogActive}
+          handleClose={handleDeleteDialogClose}
+        />
+        <Grid sx={{ padding: '15px', alignItems: 'center' }}>
+          <Card sx={{ minWidth: 345, paddingTop: '5px', paddingBottom: '5px' }}>
+            <CardContent>
+              <Typography
+                sx={{ paddingLeft: '10px', color: '#2F2F5A' }}
+                gutterBottom
+                variant="h6"
+                component="div"
+              >
+                Account Information
+              </Typography>
 
-      <ChangePasswordDialog
-        open={isPasswordDialogActive}
-        handleClose={handlePasswordDialogClose}
-      />
-      <DeleteUserDialog
-        open={isDeleteDialogActive}
-        handleClose={handleDeleteDialogClose}
-      />
-      <Grid sx={{ padding: '15px', alignItems: 'center' }}>
-        <Card sx={{ maxWidth: 345, paddingTop: '5px', paddingBottom: '5px' }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Account Information
-            </Typography>
+              <Typography
+                sx={{
+                  paddingTop: '10px',
+                  paddingLeft: '10px',
+                  color: '#2F2F5A',
+                }}
+                variant="body1"
+              >
+                Username
+              </Typography>
 
-            <Typography variant="h6">Username</Typography>
+              <Typography
+                sx={{
+                  paddingTop: '10px',
+                  paddingLeft: '10px',
+                  paddingBottom: '10px',
+                }}
+                variant="body2"
+                color="text.secondary"
+              >
+                {currentUsername}{' '}
+              </Typography>
+              <Divider variant="fullWidth" />
 
-            <Typography variant="body2" color="text.secondary">
-              {currentUsername}
-            </Typography>
-            <Divider variant="fullWidth" />
+              <Typography
+                sx={{
+                  color: '#2F2F5A',
+                  paddingTop: '10px',
+                  paddingLeft: '10px',
+                  paddingBottom: '10px',
+                }}
+                variant="body1"
+              >
+                Password
+              </Typography>
 
-            <Typography variant="h6">Password</Typography>
-
-            <Button onClick={handlePasswordDialogClickOpen} size="small">
-              Change Password
-            </Button>
-            <Divider variant="fullWidth" />
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid sx={{ padding: '15px' }}>
-        <Card sx={{ maxWidth: 345, paddingTop: '5px', paddingBottom: '5px' }}>
-          <CardContent>
-            <Button
-              onClick={handleDeleteDialogClickOpen}
-              sx={{ color: '#ff0000' }}
-              size="small"
-            >
-              Delete Account
-            </Button>
-          </CardContent>
-        </Card>
+              <Button
+                sx={{
+                  paddingTop: '10px',
+                  paddingLeft: '10px',
+                  paddingBottom: '10px',
+                }}
+                onClick={handlePasswordDialogClickOpen}
+                size="small"
+              >
+                Change Password
+              </Button>
+              <Divider variant="fullWidth" />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid sx={{ padding: '5px' }}>
+          <Card sx={{ minWidth: 345, paddingTop: '2px', paddingBottom: '2px' }}>
+            <CardContent>
+              <Button
+                onClick={handleDeleteDialogClickOpen}
+                sx={{ paddingLeft: '10px', color: '#ff0000' }}
+                size="small"
+              >
+                Delete Account
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </>
   )
