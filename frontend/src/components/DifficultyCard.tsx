5a97bde964ab57ee61082b69c139c97b0381caf8
Terence Ho <>
2022-10-16 22:34:43 +0800

    update ui

--- frontend/src/components/DifficultyCard.tsx
+++ frontend/src/components/DifficultyCard.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 import StarIcon from '@mui/icons-material/Star'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import { Box, Card, CardContent, Grid, Typography } from '@mui/material'
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Grid,
+  Typography,
+} from '@mui/material'
 type DifficultyCardProps = {
   difficulty: string
   starCount: number
@@ -12,38 +19,42 @@ const DifficultyCard = ({ difficulty, starCount }: DifficultyCardProps) => {
   return (
     <Card
       sx={{
-        paddingTop: '5px',
-        paddingBottom: '5px',
+        marginX: '1rem',
+        boxShadow: '4',
+        borderWidth: '1',
+        borderColor: 'gray.800',
       }}
     >
-      <CardContent>
-        <Grid
-          container
-          sx={{
-            direction: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            minWidth: 250,
-          }}
-        >
-          <Grid item>
-            <Typography
-              sx={{ paddingBottom: '10px', color: '#2F2F5A' }}
-              variant="body1"
-            >
-              {difficulty}
-            </Typography>
+      <CardActionArea sx={{ paddingTop: '5px', paddingBottom: '5px' }}>
+        <CardContent>
+          <Grid
+            container
+            sx={{
+              direction: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              minWidth: 250,
+            }}
+          >
+            <Grid item>
+              <Typography
+                sx={{ paddingBottom: '10px', color: '#2F2F5A' }}
+                variant="body1"
+              >
+                {difficulty}
+              </Typography>
+            </Grid>
+            <Box width="100%" />
+            <Grid item>
+              <ArrowBackIosIcon sx={{ color: '#E56E52' }} fontSize="large" />
+              {[...Array(starCount)].map((num) => (
+                <StarIcon sx={{ color: '#E56E52' }} fontSize="large" />
+              ))}
+              <ArrowForwardIosIcon sx={{ color: '#E56E52' }} fontSize="large" />
+            </Grid>
           </Grid>
-          <Box width="100%" />
-          <Grid item>
-            <ArrowBackIosIcon sx={{ color: '#E56E52' }} fontSize="large" />
-            {[...Array(starCount)].map((num) => (
-              <StarIcon sx={{ color: '#E56E52' }} fontSize="large" />
-            ))}
-            <ArrowForwardIosIcon sx={{ color: '#E56E52' }} fontSize="large" />
-          </Grid>
-        </Grid>
-      </CardContent>
+        </CardContent>
+      </CardActionArea>
     </Card>
   )
 }
