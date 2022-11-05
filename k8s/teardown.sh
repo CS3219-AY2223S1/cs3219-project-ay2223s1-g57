afc93d0e19dbd15be41b750d031236c9452776ef
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-11-06 02:07:25 +0800

    chore: teardown commands

---
+++ k8s/teardown.sh
@@ -0,0 +1,17 @@
+kubectl delete deploy/chat-service
+kubectl delete deploy/frontend
+kubectl delete deploy/matching-service
+kubectl delete deploy/mongodb
+kubectl delete deploy/question-service
+kubectl delete deploy/questionservicedb
+kubectl delete deploy/user-service 
+kubectl delete deploy/userservicedb
+
+kubectl delete service/chat-service
+kubectl delete service/frontend
+kubectl delete service/matching-service
+kubectl delete service/mongodb
+kubectl delete service/question-service
+kubectl delete service/questionservicedb
+kubectl delete service/user-service 
+kubectl delete service/userservicedb
