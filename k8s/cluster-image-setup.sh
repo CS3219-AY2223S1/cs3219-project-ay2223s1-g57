6af558ff9bafe904c61e45e4b943cfcdf7a90149
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-11-06 18:39:49 +0800

    chore: add matching-service image to k8s cluster

--- k8s/cluster-image-setup.sh
+++ k8s/cluster-image-setup.sh
@@ -2,3 +2,4 @@
 # this file loads all the images to the kind cluster
 kind load docker-image user-service --name kind-1
 kind load docker-image question-service --name kind-1
+kind load docker-image matching-service --name kind-1
