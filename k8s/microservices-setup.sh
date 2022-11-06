a011e48257ec071d04d4835605c86a1c287a0755
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-11-06 15:37:54 +0800

    chore: add question-service and db manifests

--- k8s/microservices-setup.sh
+++ k8s/microservices-setup.sh
@@ -6,3 +6,9 @@ kubectl apply -f manifests-v2/user-service-service.yaml
 
 kubectl apply -f manifests-v2/backend-ingress-controller.yaml 
 kubectl apply -f manifests-v2/backend-ingress-object.yaml  
+
+kubectl apply -f manifests-v2/questionservicedb-deployment.yaml
+kubectl apply -f manifests-v2/questionservicedb-service.yaml
+
+kubectl apply -f manifests-v2/question-service-deployment.yaml
+kubectl apply -f manifests-v2/question-service-service.yaml
