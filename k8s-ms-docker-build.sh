edbb3a7b6160698d9a3c00ec3216c21a8fcb6304
Jim Vincent Andes Engay <e0425954@u.nus.edu>
2022-11-06 15:20:40 +0800

    chore: load question-service image to cluster

--- k8s-ms-docker-build.sh
+++ k8s-ms-docker-build.sh
@@ -1,1 +1,2 @@
 docker build ./user-service -t user-service
+docker build ./question-service -t question-service
--- k8s/cluster-image-setup.sh
+++ k8s/cluster-image-setup.sh
@@ -1,3 +1,4 @@
 # pre-requisites: run k8s-ms-docker-build.sh from root directory to create all docker images
 # this file loads all the images to the kind cluster
 kind load docker-image user-service --name kind-1
+kind load docker-image question-service --name kind-1
